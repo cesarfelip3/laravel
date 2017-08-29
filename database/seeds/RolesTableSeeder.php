526c0d7e191f964530e406f2b2859ed4233491aa
Daniel Camargo <daniel@slcdevshop.com>
2017-08-29 09:12:20 -0600

    adding post project create

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -4,16 +4,10 @@ use Illuminate\Database\Seeder;
 
 class RolesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         Defender::createRole('slc');
         Defender::createRole('admin');
         Defender::createRole('user');
-        Defender::createRole('client');
     }
 }
